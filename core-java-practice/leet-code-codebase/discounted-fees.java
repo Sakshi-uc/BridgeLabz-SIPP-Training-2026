6a5691dfcdf7548079614c52b50cda8d0012549f
Sakshi Sharma <sakshiuc@outlook.com>
2026-06-09 15:47:34 +0530

    Restore Java source files from history

---
+++ core-java-practice/leet-code-codebase/discounted-fees.java
@@ -0,0 +1,9 @@
+public class Discount{
+    public static void main(String[] args) {
+        double fees = 125000;
+        double discountPerc = 10;
+        double discount = fees*discountPercent/100;
+        double finalFees = fees-discount;
+        System.out.println("The discount amount is INR " + discount + " and final discounted fee is INR " + finalFees);
+    }
+}
