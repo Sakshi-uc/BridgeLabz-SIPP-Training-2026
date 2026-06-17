8901e679ac04f75c3cb53327e0ec7b6ef29aa5c5
Sakshi Sharma <sakshiuc@outlook.com>
2026-06-17 12:10:20 +0530

    [Sakshi]add: assignment submitttion

--- core-java-practice/leet-code-codebase/ReverseInteger.java
+++ core-java-practice/leet-code-codebase/ReverseInteger.java
@@ -1,31 +1,18 @@
-class Solution {
-    public int reverse(int x) {
-        int maxi = Integer.MAX_VALUE;
-        int mini= Integer.MIN_VALUE;
-        int rev =0;
-        while(x!=0){
-            int l=x%10;
-            x=x/10;
-            if(rev>maxi/10){
-                return 0;
-            }
-            if(rev<mini/10){
-                return 0;
-            }
-            if(rev==maxi/10){
-                if(l>7){
-                    return 0;
-                }
-                
-            }
-            if(rev==mini/10){
-                if(l<-8){
-                    return 0;
-                }
-            }
-            rev=rev*10 +l;
-        }
-        return rev;
-
-    }
+public class ReverseInteger {
+	public static int reverseInteger(int x) {
+		long result=0;
+		while(x!=0) {
+			int digit=x%10;
+			result=result*10+digit;
+			if(result>Integer.MAX_VALUE||result<Integer.MIN_VALUE) {
+				return 0;
+			}
+			x/=10;
+		}
+		return (int)result;
+	}
+	public static void main(String[] args) {
+		System.out.println(reverseInteger(123));
+		System.out.println(reverseInteger(-123));
+	}
 }
